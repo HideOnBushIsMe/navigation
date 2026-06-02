056bb335a22e0a65857fbd43344846f47f24656b
HideOnBushIsMe <bangnguyen290802@gmail.com>
2026-06-02 16:54:05 +0700

    Add dark/light theme using Context API

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,13 +1,22 @@
+import { useContext } from 'react'
+import { ThemeContext } from './ThemeContext'
 export default function Navigation() {
+    const { theme, toggle, dark } = useContext(ThemeContext)
     return (
         <div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <nav
+                className="navbar navbar-expand-lg"
+                style={{
+                    backgroundColor: theme.backgroundColor,
+                    color: theme.color
+                }}
+            >
 
                 <a className="navbar-brand" href="#">Navbar</a>
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item active">
-                            <a className="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
+                            <a className="nav-link" href="#">Home<span className="sr-only">(current)</span></a>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link" href="#products">Products</a>
@@ -16,6 +25,15 @@ export default function Navigation() {
                             <a className="nav-link" href="#about">About</a>
                         </li>
                     </ul>
+                    <button
+                        onClick={toggle}
+                        style={{
+                            backgroundColor: theme.backgroundColor,
+                            color: theme.color
+                        }}
+                    >
+                        Switch Nav to {!dark ? 'Dark' : 'Light'} mode
+                    </button>
                 </div>
 
             </nav>
